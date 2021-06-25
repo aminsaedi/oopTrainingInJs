d39ff1b55b7e6088e5341b6d3fe51a1daa63fb81
amin <amin.di.sam@gmail.com>
2021-06-25 21:41:05 +0430

    Stopwatch

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-// console.log("Hello world");
-
-// Object literal syntax => {}
-
-// Factory function
-function createCircle(radius) {
-  return {
-    radius,
-    draw: function () {
-      console.log("draw");
+function Stopwatch() {
+  let duration = 0;
+  let status = 0;
+  let intervalId;
+  this.start = function () {
+    if (status === 1) throw new Error("Stopwatch aleardy started");
+    status = 1;
+    intervalId = setInterval(() => (duration += 1), 1000);
+  };
+  this.stop = function () {
+    if (status === 0) throw new Error("Stopwatch is not started");
+    status = 0;
+    clearInterval(intervalId);
+  };
+  this.reset = function () {
+    status = 0;
+    clearInterval(intervalId);
+  };
+  Object.defineProperty(this, "duration", {
+    get: function () {
+      return duration;
     },
-  };
+  });
 }
-
-// const circle = createCircle(1);
-// circle.draw();
-
-// Constructor function
-function Circle(radius) {
-  this.radius = radius;
-  this.draw = function () {
-    console.log("draw");
-  };
-}
-
-const anotherCircle = new Circle(1);
-
-anotherCircle.draw();
