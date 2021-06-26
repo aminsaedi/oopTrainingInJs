1124d086a6600fa4cf51df49c6c2b1134ded4e58
amin <amin.di.sam@gmail.com>
2021-06-26 22:41:51 +0430

    inheritance

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-function extend(Child, Parent) {
-  Child.prototype = Object.create(Parent.prototype);
-  Child.prototype.constructor = Child;
+function Shape() {}
+function Circle() {}
+
+// Prototypical inheritance
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle;
+
+function Rectangle(color) {
+  // To call the super constructor
+  Shape.call(this, color);
 }
 
-function Shape(color) {
-  this.color = color;
-}
-Shape.prototype.duplicate = function () {
-  console.log("duplicate");
+// Method overriding
+Shape.prototype.draw = function () {};
+Circle.prototype.draw = function () {
+  // Call the base implementation
+  Shape.prototype.draw.call(this);
+
+  // Do additional stuff here
 };
 
-function Circle(radius) {
-  this.radius = radius;
-}
-extend(Circle, Shape);
+// Don't create large inheritance hierarchies.
+// One level of inheritance is fine.
 
-Circle.prototype.duplicate = function () {
-  console.log("circle duplicate");
+// Use mixins to combine multiple objects
+// and implement composition in JavaScript.
+const canEat = {
+  eat: function () {},
 };
 
-function Square() {}
-extend(Square, Shape);
-
-Square.prototype.duplicate = function () {
-  console.log("square duplicate");
+const canWalk = {
+  walk: function () {},
 };
 
-const shapes = [new Square(), new Circle()];
-
-for (const shape of shapes) {
-  shape.duplicate();
+function mixin(target, ...sources) {
+  // Copies all the properties from all the source objects
+  // to the target object.
+  Object.assign(target, ...sources);
 }
+
+function Person() {}
+
+mixin(Person.prototype, canEat, canWalk);
