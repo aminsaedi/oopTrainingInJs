cb0f94625681ebde4547eeb93f984f0ce283db56
amin <amin.di.sam@gmail.com>
2021-06-26 20:35:57 +0430

    Calling super function

--- script.js
+++ script.js
@@ -1,42 +1,27 @@
-// Every object (except the root object) has a prototype (parent).
-// To get the prototype of an object:
-Object.getPrototypeOf(obj);
-
-// In Chrome, you can inspect "__proto__" property. But you should
-// not use that in the code.
-
-// To get the attributes of a property:
-Object.getOwnPropertyDescriptor(obj, "propertyName");
-
-// To set the attributes for a property:
-Object.defineProperty(obj, "propertyName", {
-  configurable: false, // cannot be deleted
-  writable: false,
-  enumerable: false,
-});
-
-// Constructors have a "prototype" property. It returns the object
-// that will be used as the prototype for objects created by the constructor.
-Object.prototype === Object.getPrototypeOf({});
-Array.prototype === Object.getPrototypeOf([]);
-
-// All objects created with the same constructor will have the same prototype.
-// A single instance of this prototype will be stored in the memory.
-const x = {};
-const y = {};
-Object.getPrototypeOf(x) === Object.getPrototypeOf(y); // returns true
-
-// Any changes to the prototype will be immediately visible to all objects
-// referencing this prototype.
-
-// When dealing with large number of objects, it's better to put their
-// methods on their prototype. This way, a single instance of the methods
-// will be in the memory.
-Circle.prototype.draw = function () {};
-
-// To get the own/instance properties:
-Object.keys(obj);
-
-// To get all the properties (own + prototype):
-for (let key in obj) {
+function Shape(color) {
+  this.color = color;
 }
+
+Shape.prototype.duplicate = function () {
+  console.log("Duplicate");
+};
+
+const shapeBase = Object.create(Shape.prototype);
+
+function Circle(radius, color) {
+  Shape.call(this, color);
+  this.radius = radius;
+}
+
+Circle.prototype = shapeBase;
+Circle.prototype.constructor = Circle;
+
+function Square(length) {
+  this.length = length;
+}
+
+Square.prototype = shapeBase;
+
+const s = new Shape();
+const c = new Circle(5, "dodgerblue");
+const m = new Square(3);
