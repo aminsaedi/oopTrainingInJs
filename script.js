1a5c447084b1965e2dcee7bdf381187335810faf
amin <amin.di.sam@gmail.com>
2021-06-28 20:52:16 +0430

    Private keys using keymaps

--- script.js
+++ script.js
@@ -1,43 +1,18 @@
-function Shape() {}
-function Circle() {}
+const _radius = new WeakMap();
+const _move = new WeakMap();
 
-// Prototypical inheritance
-Circle.prototype = Object.create(Shape.prototype);
-Circle.prototype.constructor = Circle;
+class Circle {
+  constructor(radius) {
+    _radius.set(this, radius);
+    _move.set(this, () => console.log("move"));
+  }
 
-function Rectangle(color) {
-  // To call the super constructor
-  Shape.call(this, color);
+  draw() {
+    console.log(`drawing a circle with radius : ${_radius.get(this)}`);
+    _move.get(this)();
+  }
 }
 
-// Method overriding
-Shape.prototype.draw = function () {};
-Circle.prototype.draw = function () {
-  // Call the base implementation
-  Shape.prototype.draw.call(this);
+const c = new Circle(5);
 
-  // Do additional stuff here
-};
-
-// Don't create large inheritance hierarchies.
-// One level of inheritance is fine.
-
-// Use mixins to combine multiple objects
-// and implement composition in JavaScript.
-const canEat = {
-  eat: function () {},
-};
-
-const canWalk = {
-  walk: function () {},
-};
-
-function mixin(target, ...sources) {
-  // Copies all the properties from all the source objects
-  // to the target object.
-  Object.assign(target, ...sources);
-}
-
-function Person() {}
-
-mixin(Person.prototype, canEat, canWalk);
+console.log(c);
