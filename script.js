c4e70c0ebfd8aa76f9fe0c183a44b7001402953f
amin <amin.di.sam@gmail.com>
2021-06-29 20:06:26 +0430

    Stack exercise

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-class Shape {
-  constructor(color) {
-    this.color = color;
+const _stack = new WeakMap();
+
+class Stack {
+  constructor() {
+    _stack.set(this, []);
   }
-  move() {
-    console.log("move");
+
+  push(value) {
+    const copy = [..._stack.get(this)];
+    copy.push(value);
+    _stack.set(this, [...copy]);
+  }
+
+  pop() {
+    const copy = [..._stack.get(this)];
+    if (copy.length > 0) {
+      const removed = copy.pop();
+      _stack.set(this, copy);
+      return removed;
+    } else throw new Error("Stack is empty");
+  }
+
+  peek() {
+    const copy = [..._stack.get(this)];
+    if (copy.length > 0) return copy[copy.length - 1];
+    else throw new Error("Stack is empty");
+  }
+
+  get count() {
+    const copy = [..._stack.get(this)];
+    return copy.length;
   }
 }
 
-class Circle extends Shape {
-  constructor(color, radius) {
-    super(color);
-    this.radius = radius;
-  }
-  move() {
-    super.move();
-    console.log("circle move");
-  }
-}
-
-const c = new Circle("red", 5);
-
-console.log(c);
+const stack = new Stack();
