ee5372be51075345e474a898dd9ef62fe5277e1b
amin <amin.di.sam@gmail.com>
2021-06-29 20:06:59 +0430

    classes in es6 cheat shhet

--- script.js
+++ script.js
@@ -1,35 +1,61 @@
-const _stack = new WeakMap();
-
-class Stack {
-  constructor() {
-    _stack.set(this, []);
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
   }
 
-  push(value) {
-    const copy = [..._stack.get(this)];
-    copy.push(value);
-    _stack.set(this, [...copy]);
+  // These methods will be added to the prototype.
+  draw() {}
+
+  // This will be available on the Circle class (Circle.parse())
+  static parse(str) {}
+}
+
+// Using symbols to implement private properties and methods
+const _size = Symbol();
+const _draw = Symbol();
+
+class Square {
+  constructor(size) {
+    // "Kind of" private property
+    this[_size] = size;
   }
 
-  pop() {
-    const copy = [..._stack.get(this)];
-    if (copy.length > 0) {
-      const removed = copy.pop();
-      _stack.set(this, copy);
-      return removed;
-    } else throw new Error("Stack is empty");
+  // "Kind of" private method
+  [_draw]() {}
+
+  // By "kind of" I mean: these properties and methods are essentally
+  // part of the object and are accessible from the outside. But accessing
+  // them is hard and awkward.
+}
+
+// using WeakMaps to implement private properties and methods
+const _width = new WeakMap();
+
+class Rectangle {
+  constructor(width) {
+    _width.set(this, width);
   }
 
-  peek() {
-    const copy = [..._stack.get(this)];
-    if (copy.length > 0) return copy[copy.length - 1];
-    else throw new Error("Stack is empty");
-  }
-
-  get count() {
-    const copy = [..._stack.get(this)];
-    return copy.length;
+  draw() {
+    console.log("Rectangle with width" + _width.get(this));
   }
 }
 
-const stack = new Stack();
+// WeakMaps give us better protection than symbols. There is no way
+// to access private members implemented using WeakMaps from the
+// outside of an object.
+
+// Inheritance
+class Triangle extends Shape {
+  constructor(color) {
+    // To call the base constructor
+    super(color);
+  }
+
+  draw() {
+    // Call the base method
+    super.draw();
+
+    // Do some other stuff here
+  }
+}
