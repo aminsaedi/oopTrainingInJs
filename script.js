cb7351c790a6a2a6845532d50ff1b6de76730688
amin <amin.di.sam@gmail.com>
2021-06-29 20:53:03 +0430

    modules cheat sheet

--- script.js
+++ script.js
@@ -1,61 +1,24 @@
-class Circle {
-  constructor(radius) {
-    this.radius = radius;
-  }
 
-  // These methods will be added to the prototype.
-  draw() {}
+// Module formats
+//  - AMD / Asynchronous Module Definition (Browser)
+//  - CommonJS (Node)
+//  - UMD / Universal Module Definition (Browser + Node)
+//  - ES6 Modules 
 
-  // This will be available on the Circle class (Circle.parse())
-  static parse(str) {}
-}
+// CommonJS (Used in Node)
+// Exporting 
+module.exports.Cirlce = Circle; 
+// Importing 
+const Circle = require('./circle');
 
-// Using symbols to implement private properties and methods
-const _size = Symbol();
-const _draw = Symbol();
+// ES6 Modules (Used in Browser)
+// Exporting
+export class Square {}
+// Importing 
+import {Square} from './square'; 
 
-class Square {
-  constructor(size) {
-    // "Kind of" private property
-    this[_size] = size;
-  }
+// We use Babel to transpile our modern JavaScript code 
+// into code that browsers can understand (typically ES5). 
 
-  // "Kind of" private method
-  [_draw]() {}
-
-  // By "kind of" I mean: these properties and methods are essentally
-  // part of the object and are accessible from the outside. But accessing
-  // them is hard and awkward.
-}
-
-// using WeakMaps to implement private properties and methods
-const _width = new WeakMap();
-
-class Rectangle {
-  constructor(width) {
-    _width.set(this, width);
-  }
-
-  draw() {
-    console.log("Rectangle with width" + _width.get(this));
-  }
-}
-
-// WeakMaps give us better protection than symbols. There is no way
-// to access private members implemented using WeakMaps from the
-// outside of an object.
-
-// Inheritance
-class Triangle extends Shape {
-  constructor(color) {
-    // To call the base constructor
-    super(color);
-  }
-
-  draw() {
-    // Call the base method
-    super.draw();
-
-    // Do some other stuff here
-  }
-}
+// We use Webpack to combine our JavaScript files into a
+// bundle. 
